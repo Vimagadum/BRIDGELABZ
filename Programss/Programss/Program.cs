be46c2da21ef7062b69be9688787e46fa868f756
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-01-10 13:31:18 +0530

    [vianyak]

---
+++ Programss/Programss/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Programss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Fibonaccy.findFibonacciSeries();
+        }
+    }
+}
