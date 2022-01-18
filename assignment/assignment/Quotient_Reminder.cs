f69fc786e134c2772fc19c76b3cc725b1b27ec64
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-01-18 13:54:40 +0530

    [Vinayak]  Quotient_Reminder program

---
+++ assignment/assignment/Quotient_Reminder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment
+{
+    internal class Quotient_Reminder
+    {
+        public void number()
+        {
+            Console.WriteLine("Enter a Divindend Number");
+            int Dividend = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a Diviser Number");
+            int Diviser = int.Parse(Console.ReadLine());
+
+            int Quotient = Dividend / Diviser;
+            int Reminder = Dividend % Diviser;
+
+            Console.WriteLine("Quotient is :" + Quotient);
+            Console.WriteLine("Reminder is :" + Reminder);
+
+        }
+    }
+}
