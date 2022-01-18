61ec90fb4289c687df70935b278a0e4d64e95d78
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-01-18 13:59:55 +0530

    [Vinayak] checking alphabat is vowel or consonant

---
+++ assignment/assignment/VowelorConsonant.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment
+{
+    public class VowelorConsonant
+    {
+        public void charecter()
+        {
+            char charrr;
+
+            Console.WriteLine("Enter alphabet to check vowel or consonant ");
+            
+            charrr = Convert.ToChar(Console.ReadLine().ToLower());
+            
+            
+                switch (charrr)
+                {
+                    case 'a':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'i':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'o':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'u':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    case 'e':
+                        Console.WriteLine("The Alphabet is vowel");
+                        break;
+                    default:
+                        Console.WriteLine("The Alphabet is Consonant");
+                        break;
+                }
+            
+            
+
+        }
+    }
+}
