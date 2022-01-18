f513c89f80682090cede2260e85deb1ecb810111
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-01-18 13:51:30 +0530

    [Vinayak] adding leap year program

---
+++ assignment/assignment/Leap_Year.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment
+{
+    public class Leap_Year
+    {
+           public void year()
+           {
+                Console.WriteLine("Enter year");
+                int yearis = int.Parse(Console.ReadLine());
+
+                if (yearis.ToString().Length != 4)
+                {
+                    Console.WriteLine("Please enter valid year");
+                }
+                else if (((yearis % 4 == 0) && (yearis % 100 != 0)) || (yearis % 400 == 0))
+                {
+                    Console.WriteLine(" " + yearis + "Year is Leap Year");
+                }
+                else
+                {
+                    Console.WriteLine(" " + yearis + "Year is not Leap Year");
+                }
+           }
+
+    }
+}
