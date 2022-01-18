0ea632914d58b7ff6451cb01f661f9fbb7a56086
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-01-18 13:58:12 +0530

    [Vinayak] checking a number Even or Odd

---
+++ assignment/assignment/Even_Odd.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment
+{
+    public class Even_Odd
+    {
+        public void number()
+        {
+            Console.WriteLine("Enter Number");
+            int Number = int.Parse(Console.ReadLine());
+
+            if (Number <= 0)
+            {
+                Console.WriteLine("Please enter positive number");
+            }
+            else if (Number % 2 == 0)
+            {
+                Console.WriteLine(Number+" It is Even Number");
+            }
+            else
+            {
+                Console.WriteLine( Number + " It is Odd Number");
+            }
+        }
+    }
+}
