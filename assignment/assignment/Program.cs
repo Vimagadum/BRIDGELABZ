600e48fa3a9bb8e1f20981e4a813277757018c40
Vimagadum <95463195+Vimagadum@users.noreply.github.com>
2022-01-18 14:11:13 +0530

    [Vinayak] adding progrmas

---
+++ assignment/assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using assignment;
+Console.WriteLine("Hello, World!");
+//Leap_Year lp = new Leap_Year();
+//lp.year();
+
+//Even_Odd Evd = new Even_Odd();
+//Evd.number();
+//Quotient_Reminder qr = new Quotient_Reminder();
+//qr.number();
+
+//VowelorConsonant vc = new VowelorConsonant();
+//vc.charecter();
+
+LargestNumber ln = new LargestNumber();
+ln.number();
